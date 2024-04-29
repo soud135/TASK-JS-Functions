98f6349aea3228a5b14e5654433649516cc5d1fd
soud <soud9949ale@gmail.com>
2024-04-29 19:16:02 +0300

    done

--- challenge1.js
+++ challenge1.js
@@ -3,6 +3,11 @@
  * Create a function named `printName`
  * - that just prints your name on the screen
  */
+function printName() {
+    console.log("soud alenezi")
+}
+printName()
+
 
 /**
  * Task 2:
@@ -11,7 +16,12 @@
  * - and prints the age on the screen.
  * - Age = current year - birth
  */
+function printAge(year){
+    console.log(2024-year)
 
+
+}
+printAge(2011)
 /**
  * Task 3:
  * Create a function named `printHello`
@@ -22,10 +32,27 @@
  * -- fr: it should print `Bonjour NAME`
  * -- tr: it should print `Merhaba NAME`
  */
-
+function printHello(name,language){
+    if(language==="en"){
+    console.log(`Hola ${name}`)
+} else if(language==="es"){
+    console.log(`Bonjour ${name}`)
+} else if(language==="fr"){
+    console.log(`hello ${name}`)
+} else if(language==="tr"){
+    console.log(`Merhaba ${name}`)
+}
+}
 /**
  * Task 4:
  * Create a function named `printMax`
  * - that takes 2 parameters as numbers
  * - should print out the bigger number
- */
+ */function printMax(num1,num2){
+if (num1>num2) {
+    console.log(num1)
+    
+}else{
+    console.log(num2)
+}
+ }
